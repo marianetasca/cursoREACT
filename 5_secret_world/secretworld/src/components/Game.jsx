b35c166b979897bf203e46861fae9f0e686bccb6
Mariane <mariane012.12@gmail.com>
2026-04-04 21:35:05 -0300

    Projeto Secret Word

---
+++ 5_secret_world/secretworld/src/components/Game.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useState, useRef } from "react";
+import "./Game.css";
+
+const Game = ({
+  verifyLetter,
+  // pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter); // para passar valor para o app
+
+    setLetter("");
+
+    letterInputRef.current.focus(); // quando manda a letra ele volta para o campo auto
+  };
+  return (
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          ),
+        )}
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="letter"
+            maxLength="1"
+            required
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersConatiner">
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter, i) => (
+          <span key={i}>{letter},</span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Game;
